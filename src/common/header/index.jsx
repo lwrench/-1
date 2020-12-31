4bed9123c5e92b0822ee7f3eebb7ecded76b40b9
lwrench <1243115181@qq.com>
2020-12-31 22:24:37 +0800

    header

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -13,9 +13,21 @@ class Header extends Component {
         <div className="header-nav">
           <div className="header-nav-item left active">首页</div>
           <div className="header-nav-item left">下载App</div>
-          <input className="search" type="text" placeholder="search"/>
+          <div className="searchWrapper">
+            <input className="search" type="text" placeholder="search"/>
+            <i className="iconfont">&#xe614;</i>
+          </div>
           <div className="header-nav-item right">登陆</div>
-          <div className="header-nav-item right">Aa</div>
+          <div className="header-nav-item right">
+            <i className="iconfont">&#xe636;</i>
+          </div>
+        </div>
+        <div className="header-addition">
+          <button className="header-addition-btn reg">
+            <i className="iconfont" style={{marginRight: '5px'}}>&#xe6e5;</i>
+             写文章
+          </button>
+          <button className="header-addition-btn writing">注册</button>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './style.scss'
+import './static/iconfont/iconfont.css'
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
